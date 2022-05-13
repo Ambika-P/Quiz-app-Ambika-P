9756ff7727eaff8a1b544cd4aa37f1c03db099e8
Ambika-P <91551332+Ambika-P@users.noreply.github.com>
2022-05-13 15:53:42 +0530

    Add files via upload

---
+++ src/pages/finalResult.js
@@ -0,0 +1,67 @@
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+import Questions from "./Questions";
+function App() {
+  // Properties
+  const [showResults, setShowResults] = useState(false);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const optionClicked = (isCorrect) => {
+    // Increment the score
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setShowResults(true);
+    }
+  };
+  return (
+    <div className="App">
+      
+      <h1>USA Quiz 🇺🇸</h1>
+
+      
+      <h2>Score: {score}</h2>
+
+      
+      {showResults ? (
+
+<div className="final-results">
+          <h1>Final Results</h1>
+          <h2>
+            {score} out of {questions.length} correct - (
+            {(score / questions.length) * 100}%)
+          </h2>
+          <button onClick={() => restartGame()}>Restart game</button>
+        </div>
+      ) : (
+        /* 5. Question Card  */
+        <div className="question-card">
+          {/* Current Question  */}
+          <h2>
+            Question: {currentQuestion + 1} out of {questions.length}
+          </h2>
+          <h3 className="question-text">{questions[currentQuestion].text}</h3>
+
+          {/* List of possible answers  */}
+          <ul>
+            {questions[currentQuestion].options.map((option) => {
+              return (
+                <li
+                  key={option.id}
+                  onClick={() => optionClicked(option.isCorrect)}
+                >
+                  {option.text}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+export default finalResults;
---
+++ src/pages/navigate.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import CustomButton from './CustomButton';
+
+const MyComponents = {
+    DatePicker: function DatePicker(props) {
+      return <div>{props.color}1</div>;
+    },
+    DatePicker: function DatePicker(props) {
+        return <div>{props.color}2</div>;
+      },
+      DatePicker: function DatePicker(props) {
+        return <div>{props.color}3</div>;
+      },
+      DatePicker: function DatePicker(props) {
+        return <div>{props.color}4</div>;
+      }
+  }
+  
+  function BlueDatePicker() {
+    return <MyComponents.DatePicker[ifAnswered] color="green />;
+    return <MyComponents.DatePicker[Unanswered] color="grey />;
+    return <MyComponents.DatePicker[CurrentQuestion] color="yellow/>;
+  }
